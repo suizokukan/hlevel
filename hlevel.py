c9559f3bf665abd7bd4ae929a2020171c7bdbc2a
suizokukan <suizokukan@orange.fr>
2013-10-15 17:41:23 +0200

    hlevel.py : missing 'g' in lowercaseletter_symbols

--- hlevel.py
+++ hlevel.py
@@ -100,7 +100,7 @@ class HLevel(list):
                              "J", "K", "L", "M", "N", "O", "P", "Q", "R", \
                              "S", "T", "U", "V", "W", "X", "Y", "Z",)
 
-    lowercaseletter_symbols = ("a", "b", "c", "d", "e", "f", "h", "j", "i", \
+    lowercaseletter_symbols = ("a", "b", "c", "d", "e", "f", "g", "h", "i", \
                              "j", "k", "l", "m", "n", "o", "p", "q", "r", \
                              "s", "t", "u", "v", "w", "x", "y", "z",)
 
